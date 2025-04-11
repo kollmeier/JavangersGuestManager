d1188232fea6f082442f2630e3260faef078db1c
Carsten Kollmeier <carsten@ckollmeier.de>
2025-04-11 11:13:09 +0200

    Add test for GuestList writing to file system
    Introduced a new test to verify that `setGuests` writes guest data to the file system. This ensures the functionality works correctly and improves test coverage. AssertJ library was also imported for better assertion capabilities.

--- src/test/java/ckollmeier/de/GuestListTest.java
+++ src/test/java/ckollmeier/de/GuestListTest.java
@@ -2,10 +2,12 @@ package ckollmeier.de;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class GuestListTest {
     @Test
@@ -22,5 +24,19 @@ class GuestListTest {
         guestList.setGuests(new ArrayList<>(List.of("Karl", "Ute")));
 
         assertEquals(List.of("Karl", "Ute"), guestList.getGuests());
+    }
+
+    @Test
+    void setGuestsShouldWriteToFileSystem() {
+        GuestList guestList = GuestList.builder().build();
+
+        guestList.setGuests(new ArrayList<>(List.of("Theodor", "Anette")));
+
+        assertTrue(Files.exists(GuestList.PATH));
+        try {
+            assertThat(Files.readAllLines(GuestList.PATH)).containsExactly("Theodor", "Anette");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
