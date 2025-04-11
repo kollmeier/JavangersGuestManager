e2427eee4c7d132223049733bebdaca90a2dd4ca
Carsten Kollmeier <carsten@ckollmeier.de>
2025-04-11 11:13:01 +0200

    Add PATH constant for guest list file location
    Introduced a static constant `PATH` to define the file location for the guest list. This improves maintainability by centralizing the file path reference.

--- src/main/java/ckollmeier/de/GuestList.java
+++ src/main/java/ckollmeier/de/GuestList.java
@@ -1,15 +1,17 @@
 package ckollmeier.de;
 
 import lombok.Builder;
-import lombok.NoArgsConstructor;
 import lombok.With;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 @Builder
 @With
 public class GuestList {
+    public static final Path PATH = Path.of("guests.txt");
+
     private final List<String> guests = new ArrayList<>();
 
     public void setGuests(List<String> guests) {
